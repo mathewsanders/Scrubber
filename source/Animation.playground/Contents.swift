38756e083e50d111fcc7a9ff218b7fcdb54a39a8
Mathew Sanders <mathew.sanders@gmail.com>
2017-03-14 14:12:42 -0400

    Fix keyframe bug.

--- source/Animation.playground/Contents.swift
+++ source/Animation.playground/Contents.swift
@@ -11,33 +11,33 @@ container.addSubview(square)
 
 container.startState = {
     square.transform = .identity
-    square.bounds.size = CGSize(width: 100, height: 50)
+    square.bounds.size = CGSize(width: 150, height: 50)
     square.backgroundColor = .red
     square.center = container.center
 }
 
 container.animator = {
-    let animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeIn)
+    
+    
+    let animator = UIViewPropertyAnimator(duration: 2.5, curve: .easeInOut)
     
     animator.addAnimations {
         
-        UIView.animateKeyframes(withDuration: 1, delay: 0, options: [.calculationModePaced], animations: {
+        UIView.animateKeyframes(withDuration: 1.0, delay: 0, options: [], animations: {
             
-            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.75) {
-                
-                square.backgroundColor = .yellow
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.25) {
+                square.backgroundColor = .blue
             }
             
-            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5) {
-            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1.0) {
                 square.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.5) {
+                square.bounds.size = CGSize(width: 50, height: 150)
             }
         })
     }
     
     return animator
 }
-
-
-
-
